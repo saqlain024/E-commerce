bb5a1552a2462e66744b843898490f5001c02535
Saklain Ahmad <saklainhmd@gmail.com>
2024-01-09 00:35:29 +0530

    first commit : Initialize project using react app && Home Page Template Added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import React from 'react';
+import Counter from './features/counter/Counter.js'
+import './App.css';
+import Home from './pages/Home.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
